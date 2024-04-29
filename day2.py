d85bf463f50772d7ef8e7b97bcf244ac1efbf996
marvin evins <marvinevins@yahoo.com>
2024-04-29 08:42:56 -0500

    learning basics

--- day2.py
+++ day2.py
@@ -1,47 +1,65 @@
-# Description: This file is for the second day of the python workshop
+# # Description: This file is for the second day of the python workshop
 
 
-# create variables for the following :
-# 1. age
-# 2. name
-# 3. song
-# 4. food
-# 5. number
-# #now include the variables you just made print in the following...
-# Once upon a time, there was a [age] old coder named [name].
-# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would
-# throw [food] until [name] would stop singing.
-# Still, [name] was the best coder on the team and could write [number] lines of code every day.
-# Maybe [song] was [name]’s secret power?
-# No one will ever know.
+# # create variables for the following :
+# # 1. age
+# # 2. name
+# # 3. song
+# # 4. food
+# # 5. number
+# # #now include the variables you just made print in the following...
+# # Once upon a time, there was a [age] old coder named [name].
+# # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would
+# # throw [food] until [name] would stop singing.
+# # Still, [name] was the best coder on the team and could write [number] lines of code every day.
+# # Maybe [song] was [name]’s secret power?
+# # No one will ever know.
 
 
-# What is syntax ? What is an algorithm?
-# what is a variable? What is a string?
+# # What is syntax ? What is an algorithm?
+# # what is a variable? What is a string?
 
-# strings are nothing but plain text
-# what does this do?
-print("Giraffe \n academy")
-
-# or this
-phrase = "python learning"
-print(phrase + "is cool")
-#what does the + sign do? What is it called?
+# # strings are nothing but plain text
+# # what does this do?
+# from os import name
 
 
-#what if I wanted to get the length of a phrase?
+# print("Giraffe \n academy")
+
+# # or this
+# phrase = "python learning"
+# print(phrase + "is cool")
+# #what does the + sign do? What is it called?
+
+
+# #what if I wanted to get the length of a phrase?
 
 
 #what if I wanted to make the letters in the variable upper case or lower?
-
-
+name = "welcome to day 2 part 3"
+print(f'the length is {len(name)}')
+print(name.upper())
+print(name.lower())
 
 
 #what if I wanted to check and see if the phrase was all lower or upper?
-
+print(name.islower())#returns a boolean true or false
+print(name.isupper())#returns a boolean true or false
 
 #What if I wanted to get one letter of the phrase
-
+phrase2 = "fallout is a great show"
+print(phrase2[0])#prints the first letter of the string
+print(phrase2[1])
+print(phrase2[5])# prints the 6th letter of the string
+print(phrase2[13])#prints the index of the first occurence of the letter a
+#string slicing if I wanted to get
+#entire word
+print(phrase2[13:18])#prints the first 7 letters of the string
+print(phrase2[19:23])#prints the first 7 letters of the string
+#fallout
+print(phrase2[0:7])#prints the index of the first occurence of the letter a
+#get the last letter of the string
+print(phrase2[-1])#prints the last letter of the string
 
 # The names you use when creating these labels need to follow a few rules:
 # 1. Names can not start with a number.
@@ -53,7 +71,7 @@ print(phrase + "is cool")
 
 
 
-# Working with numbers bold text
+# Working with numbers 
 # We'll learn about the following topics:
 # 1. Types of Numbers in Python
 # 2. Basic Arithmetic
@@ -61,21 +79,21 @@ print(phrase + "is cool")
 
 
 # Addition
-
+print(2 + 2)
 # Subtraction
-
+print(2 - 5)
 # Multiplication
-
+print(2 * 3)
 # Division
-
+print(10 / 2)
 # Modulus
-
+print(10 % 3)#prints the remainder of the division
 # Exponentiation
-
+print(2 ** 3)# 2 to the power of 3
 # Floor Division
-
+print(10 // 3)# 10 divided by 3 is 3 with a remainder of 1
 # Order of Operations followed in Python
-
+print(2 * 3 + 1)# 7
 # You can use parentheses to specify the order in which you want operations to be performed.
 
 #to do more you need to import special math libraries from python
---
+++ day3.py
@@ -0,0 +1,6 @@
+#booleans
+print(10 > 9) # is this true or false?
+# booleans are used to evaluate expressions and conditions in python
+# they are used to check if an expression is true or false
+broughtFood = True
+print(broughtFood)
