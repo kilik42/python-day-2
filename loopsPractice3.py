1515ec8f2a60f7dc9499fe812a943fe00b6ffa6c
marvin evins <marvinevins@yahoo.com>
2024-05-09 08:38:17 -0500

    loop practice

---
+++ loopsPractice3.py
@@ -0,0 +1,12 @@
+#loops repeat a block of code multiple times
+#while loop repeats a block of code as long as a condition is true
+#For loop repeats a block of code a specific number of times
+list1 = range(1,1001)
+# print(list1)
+for number in list1:
+     if number % 2 == 0:
+         print(f'number is even {number}')
+     else:
+        print("This number is not even")
+#sum
+print(f'the sum of the list is {sum(list1)}')
